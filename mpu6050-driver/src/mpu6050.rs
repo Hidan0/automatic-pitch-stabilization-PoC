76fe9fed84d358c09a34f4e5956cf3c444da81b4
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-06-06 18:26:38 +0200

    [ADD] mpu6050 calibration

--- mpu6050-driver/src/mpu6050.rs
+++ mpu6050-driver/src/mpu6050.rs
@@ -24,10 +24,16 @@ const GYRO_REGZ_H: u8 = 0x47;
 /// Power management register
 const PWR_MGMT_ADDR: u8 = 0x6B;
 
+/// Maximum number of calibrations
+const MAX_CALIBRATIONS: usize = 2000;
+
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct Mpu6050<I2C> {
     i2c: I2C,
+    rate_cal_roll: f32,
+    rate_cal_pitch: f32,
+    rate_cal_yaw: f32,
 }
 
 impl<I2C, E> Mpu6050<I2C>
@@ -36,13 +42,45 @@ where
 {
     /// Create a new instance of the Mpu6050
     pub fn new(i2c: I2C) -> Result<Self, E> {
-        Ok(Self { i2c })
+        Ok(Self {
+            i2c,
+            rate_cal_roll: 0.,
+            rate_cal_pitch: 0.,
+            rate_cal_yaw: 0.,
+        })
     }
 
     pub fn init<D: DelayMs<u8>>(&mut self, delay: &mut D) -> Result<(), E> {
+        self.start_up(delay)?;
+        self.configure()?;
+        self.calibrate(delay)?;
+        Ok(())
+    }
+
+    fn start_up<D: DelayMs<u8>>(&mut self, delay: &mut D) -> Result<(), E> {
         delay.delay_ms(250);
         self.write_byte(PWR_MGMT_ADDR, 0x00)?;
         delay.delay_ms(100);
+        Ok(())
+    }
+
+    fn calibrate<D: DelayMs<u8>>(&mut self, delay: &mut D) -> Result<(), E> {
+        for _ in 0..MAX_CALIBRATIONS {
+            let (r, p, y) = self.raw_gyro()?;
+            self.rate_cal_roll += r;
+            self.rate_cal_pitch += p;
+            self.rate_cal_yaw += y;
+            delay.delay_ms(1);
+        }
+
+        self.rate_cal_roll /= MAX_CALIBRATIONS as f32;
+        self.rate_cal_pitch /= MAX_CALIBRATIONS as f32;
+        self.rate_cal_yaw /= MAX_CALIBRATIONS as f32;
+
+        Ok(())
+    }
+
+    fn configure(&mut self) -> Result<(), E> {
         // switch on the low pass filter
         self.write_byte(DLPF_ADDR, DLPF_CFG_GYR_5)?;
 
@@ -51,16 +89,25 @@ where
         Ok(())
     }
 
-    pub fn gyro(&mut self) -> Result<(f32, f32, f32), E> {
-        let x = self.read_2c_word(GYRO_REGX_H)?;
-        let y = self.read_2c_word(GYRO_REGY_H)?;
-        let z = self.read_2c_word(GYRO_REGZ_H)?;
+    fn raw_gyro(&mut self) -> Result<(f32, f32, f32), E> {
+        let r = self.read_2c_word(GYRO_REGX_H)?;
+        let p = self.read_2c_word(GYRO_REGY_H)?;
+        let y = self.read_2c_word(GYRO_REGZ_H)?;
 
         // converts the measurements to °/s
         Ok((
-            x as f32 / GYR_500_LSB_SENS,
+            r as f32 / GYR_500_LSB_SENS,
+            p as f32 / GYR_500_LSB_SENS,
             y as f32 / GYR_500_LSB_SENS,
-            z as f32 / GYR_500_LSB_SENS,
+        ))
+    }
+
+    pub fn gyro(&mut self) -> Result<(f32, f32, f32), E> {
+        let (r, p, y) = self.raw_gyro()?;
+        Ok((
+            r - self.rate_cal_roll,
+            p - self.rate_cal_pitch,
+            y - self.rate_cal_yaw,
         ))
     }
 
