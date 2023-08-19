de08a915a155ff39ef084d17c6f732b6d8fc52df
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-08-19 12:08:57 +0200

    test,relazione: eseguiti test per la calibrazione, aggiunta relazione

--- gyro-controls/src/main.rs
+++ gyro-controls/src/main.rs
@@ -1,13 +1,16 @@
 use embedded_hal::blocking::delay::DelayMs;
-use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::delay::{self, FreeRtos};
 use esp_idf_hal::gpio::{Gpio6, Output, PinDriver};
 use esp_idf_hal::i2c::{I2cConfig, I2cDriver};
 use esp_idf_hal::prelude::*;
 use esp_idf_sys as _;
-use mpu6050::Mpu6050; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use log::info;
+use mpu6050::Mpu6050;
+use std::time::SystemTime;
 
 fn main() {
     esp_idf_sys::link_patches();
+    esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = Peripherals::take().unwrap();
 
@@ -17,21 +20,48 @@ fn main() {
     let scl = peripherals.pins.gpio5;
     let sda = peripherals.pins.gpio4;
 
-    let config = I2cConfig::new().baudrate(115200.Hz());
+    let config = I2cConfig::new().baudrate(400000.Hz());
     let i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &config).unwrap();
 
     let mut mpu = setup_mpu(&mut FreeRtos, i2c);
 
+    let start_time = SystemTime::now();
+    info!("Starting calibration...");
+    let (cal_r, _, _) = calibrate_gyro(&mut delay::Delay, &mut mpu, &mut cal_led);
+    let end_time = SystemTime::now();
+    info!(
+        "Finished calibration, {}ms",
+        end_time.duration_since(start_time).unwrap().as_millis()
+    );
+
     loop {
-        println!("{}", mpu.get_gyro().unwrap().x);
+        println!("{}", mpu.get_gyro().unwrap().x - cal_r);
         FreeRtos::delay_ms(50);
     }
 }
 
-#[allow(dead_code)]
-fn calibrate_gyro<D: DelayMs<u8>>(delay: &mut D, info_led: &mut PinDriver<Gpio6, Output>) {
-    delay.delay_ms(150);
+fn calibrate_gyro<D: DelayMs<u16>>(
+    delay: &mut D,
+    mpu: &mut Mpu6050<I2cDriver>,
+    info_led: &mut PinDriver<Gpio6, Output>,
+) -> (f32, f32, f32) {
+    let (mut cal_r, mut cal_p, mut cal_y) = (0., 0., 0.);
+    const NUMBER_CAL: usize = 500;
+
+    for _ in 0..NUMBER_CAL {
+        cal_r += mpu.get_gyro().unwrap().x;
+        cal_p += mpu.get_gyro().unwrap().y;
+        cal_y += mpu.get_gyro().unwrap().z;
+        delay.delay_ms(1);
+    }
+
     info_led.set_low().unwrap();
+
+    (
+        cal_r / NUMBER_CAL as f32,
+        cal_p / NUMBER_CAL as f32,
+        cal_y / NUMBER_CAL as f32,
+    )
 }
 
 fn setup_mpu<D: DelayMs<u8>>(
