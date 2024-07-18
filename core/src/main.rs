b280bffefb387c4788ce490f950144f174e4c835
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2024-07-19 00:53:34 +0200

    add, demo per calcolare errore medio della conversione

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,4 +1,3 @@
-use esp_idf_svc::hal::delay::FreeRtos;
 use esp_idf_svc::hal::peripherals::Peripherals;
 use servo::ServoSG90;
 
@@ -16,24 +15,28 @@ fn main() -> Result<()> {
     let mut servo = ServoSG90::new(
         peripherals.ledc.channel0,
         peripherals.ledc.timer0,
-        peripherals.pins.gpio7,
+        peripherals.pins.gpio6,
     )?;
 
-    loop {
-        for i in 0..180 {
+    let trials = 10;
+    let mut tot = 0;
+
+    for _ in 0..trials {
+        for i in -90..=90 {
             servo.write_angle(i)?;
-
-            log::info!("Writing angle {}", i);
-            log::info!("Reading angle {}\n", servo.read_exp_angle());
-
-            FreeRtos::delay_ms(15)
+            tot += i - servo.read_exp_angle();
         }
 
-        for i in (0..180).rev() {
+        for i in (-90..=90).rev() {
             servo.write_angle(i)?;
-            FreeRtos::delay_ms(15)
+            tot += i - servo.read_exp_angle();
         }
-
-        FreeRtos::delay_ms(10000);
     }
+
+    println!(
+        "Average conversion error: {}",
+        tot as f32 / (trials * 360) as f32
+    );
+
+    Ok(())
 }
