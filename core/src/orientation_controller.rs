482e73b4e998b4cb1656dd1cd69886950b712e7b
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2024-09-09 18:25:12 +0200

    feat(orientation): calibration function and get roll

--- core/src/orientation_controller.rs
+++ core/src/orientation_controller.rs
@@ -1,3 +1,5 @@
+use std::time::SystemTime;
+
 use embedded_hal::delay::DelayNs;
 use esp_idf_svc::hal::delay::FreeRtos;
 use esp_idf_svc::hal::i2c::I2cDriver;
@@ -8,8 +10,15 @@ use crate::Result;
 
 type MpuDriver<'a> = Mpu6050<I2cDriver<'a>>;
 
+const CALIBRATIONS: u16 = 2000;
+
 pub struct Controller<'a> {
     mpu: MpuDriver<'a>,
+    pub calibrations: GyroCalibration,
+}
+
+pub struct GyroCalibration {
+    r: f32,
 }
 
 impl<'a> Controller<'a> {
@@ -25,12 +34,37 @@ impl<'a> Controller<'a> {
         Ok(mpu)
     }
 
+    fn calibrate_gyro(mpu: &mut MpuDriver<'a>) -> GyroCalibration {
+        let mut r = 0_f32;
+
+        let start_time = SystemTime::now();
+        for _ in 0..CALIBRATIONS {
+            r += mpu.get_gyro().unwrap().x;
+        }
+        let current = SystemTime::now();
+        log::info!(
+            "For {} measurements took {}ms",
+            CALIBRATIONS,
+            current
+                .duration_since(start_time)
+                .unwrap_or_default()
+                .as_millis()
+        );
+
+        GyroCalibration {
+            r: r / CALIBRATIONS as f32,
+        }
+    }
+
     pub fn new(i2c: I2cDriver<'a>) -> Result<Controller<'a>> {
+        let delay = &mut FreeRtos;
         log::info!("Starting Mpu set up...");
-        let mpu = Self::setup_mpu(&mut FreeRtos, i2c)?;
+        let mut mpu = Self::setup_mpu(delay, i2c)?;
         log::info!("Finished Mpu set up.");
 
-        Ok(Self { mpu })
+        let calibrations = Self::calibrate_gyro(&mut mpu);
+
+        Ok(Self { mpu, calibrations })
     }
 
     pub fn get_roll(&mut self) -> Result<f32> {
